367f7c8e810d74ee84131a907835a936ed384fc5
tristan-rnd <107555726+tristan-rnd@users.noreply.github.com>
2024-01-28 16:52:40 +0100

    Added header

--- interface.py
+++ interface.py
@@ -1,3 +1,10 @@
+# --------------------------------------------------------------------------------
+# Auteurs : Nassim HAMRI & Tristan RENAUD
+# Date : Janvier 2024
+# 
+# Ce fichier est partie intégrante de Stent_design_and_construction écrit par les mêmes auteurs.
+# --------------------------------------------------------------------------------
+
 import tkinter as tk
 from tkinter import filedialog, simpledialog, messagebox
 from classStent import Stent
